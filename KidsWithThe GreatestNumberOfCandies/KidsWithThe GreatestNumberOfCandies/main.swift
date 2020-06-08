96ee8bfee7b5e74b702edf29f039d80e8d7636af
Dan Morse <djmthedev@gmail.com>
2020-06-08 12:07:49 -0700

    Implemented KidsWithTheGreatestNumberOfCandies

--- KidsWithThe GreatestNumberOfCandies/KidsWithThe GreatestNumberOfCandies/main.swift
+++ KidsWithThe GreatestNumberOfCandies/KidsWithThe GreatestNumberOfCandies/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
+class Solution {
+    func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
+        guard let maxCandies = candies.max() else { return [] }
+        var canBeMax: [Bool] = []
+        for number in candies {
+            if number + extraCandies >= maxCandies {
+                canBeMax.append(true)
+            } else {
+                canBeMax.append(false)
+            }
+        }
+        return canBeMax
+    }
+}
+
+let s = Solution()
+var candies = [4,2,1,1,2]
+var extraCandies = 1
+
+print(s.kidsWithCandies(candies, extraCandies)) // [true,false,false,false,false]
+
+candies = [12,1,12]
+extraCandies = 10
+print(s.kidsWithCandies(candies, extraCandies)) // [true,false,true]
