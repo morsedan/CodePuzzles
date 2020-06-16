eb8b314909f00784351258e30c9273a8fb714f6e
Dan Morse <djmthedev@gmail.com>
2020-06-16 09:07:09 -0700

    implemented ThirdMaximumNumber

--- ThirdMaximumNumber/ThirdMaximumNumber/main.swift
+++ ThirdMaximumNumber/ThirdMaximumNumber/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+ options:
+ - sort, then count backwards until getting to third unique number
+ - eliminate duplicates, sort, then get third from end
+ */
 
+class Solution {
+    func thirdMax(_ nums: [Int]) -> Int {
+        if nums.count < 3 {
+            return nums.max()!
+        }
+        
+        let sortedNumbers = nums.sorted()
+        var nthDistinct = 1
+        var index = sortedNumbers.count - 1
+        var previousMax = sortedNumbers[index]
+        
+        while index >= 0 {
+            if sortedNumbers[index] != previousMax {
+                nthDistinct += 1
+                previousMax = sortedNumbers[index]
+            }
+            if nthDistinct == 3 {
+                return sortedNumbers[index]
+            }
+            index -= 1
+        }
+        return nums.max()!
+    }
+}
+
+let s = Solution()
+
+let numbers1 = [3, 2, 1] // 1
+let numbers2 = [1, 2] // 2
+let numbers3 = [2, 2, 3, 1] // 1
+
+print(s.thirdMax(numbers1))
+print(s.thirdMax(numbers2))
+print(s.thirdMax(numbers3))
