8074a2d3427c11c450d0bf2c2b348d4d4515c41c
Dan Morse <djmthedev@gmail.com>
2020-07-13 09:09:29 -0700

    added GroupThePeopleGivenTheGroupSizeTheyBelongTo

---
+++ GroupThePeopleGivenTheGroupSizeTheyBelongTo/GroupThePeopleGivenTheGroupSizeTheyBelongTo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GroupThePeopleGivenTheGroupSizeTheyBelongTo
+//
+//  Created by morse on 7/13/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
