72acbd962a9c33459972aa5f42354a2df3efadc5
Dan Morse <djmthedev@gmail.com>
2020-07-25 08:48:37 -0700

    implemented PartitionLabels

--- PartitionLabels.py
+++ PartitionLabels.py
@@ -47,6 +47,20 @@ class Solution:
         return [len(partition) for partition in partitions]
 
 
+class TheSolution(object):
+    def partitionLabels(self, S):
+        last = {c: i for i, c in enumerate(S)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i - anchor + 1)
+                anchor = i + 1
+            
+        return ans
+
+
 
 
 s = Solution()
