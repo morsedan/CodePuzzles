327912e02522306cfd9a71e998b76f04ed7e2778
Dan Morse <djmthedev@gmail.com>
2020-09-05 15:05:52 -0700

    implemented ShortestWord

--- ShortestWord/ShortestWord/main.swift
+++ ShortestWord/ShortestWord/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
+func find_short(_ str: String) -> Int {
+    return str.split(separator: " ").sorted { $0.count < $1.count }[0].count
+}
 
+func testExample() {
+    
+    assert(3 == find_short("bitcoin take over the world maybe who knows perhaps"))
+    assert(3 == find_short("lets talk about javascript the best language"))
+    assert(1 == find_short("i want to travel the world writing code one day"))
+    assert(2 == find_short("Lets all go on holiday somewhere very cold"))
+    print("Passed")
+}
+testExample()
