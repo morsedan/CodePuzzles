eb91bd697b2e54aed5936a511b09ec7438f7c7e6
Dan Morse <djmthedev@gmail.com>
2020-10-03 10:13:47 -0700

    implemented Mumbling

--- Mumbling/Mumbling/main.swift
+++ Mumbling/Mumbling/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func accum(_ s: String) -> String {
+    var result = ""
+    
+    for (index, letter) in s.enumerated() {
+        result.append(letter.uppercased())
+        for _ in 0..<index {
+            result.append(letter.lowercased())
+        }
+        if s.count - 1 > index {
+            result.append("-")
+        }
+    }
+    return result
+}
 
+func testAccum() {
+    assert(accum("abcd") == "A-Bb-Ccc-Dddd")
+    assert(accum("cwAt") == "C-Ww-Aaa-Tttt")
+    assert(accum("RqaEzty") == "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")
+}
+testAccum()
+print("passed!")
