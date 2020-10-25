fd0eb62bac96129cde08b00cd07b895bc17c2981
Dan Morse <djmthedev@gmail.com>
2020-10-25 09:31:03 -0700

    refactored SumOfDigits to a recursive solution

--- SumOfDigits/SumOfDigits/main.swift
+++ SumOfDigits/SumOfDigits/main.swift
@@ -8,17 +8,29 @@
 
 import Foundation
 
+//func digitalRoot(of number: Int) -> Int {
+//    var number = number
+//
+//    while number >= 10 {
+//        number = sumDigit(number)
+//    }
+//
+//    return number
+//}
+//
+//func sumDigit(_ number: Int) -> Int {
+//    var digitSum = 0
+//    var number = number
+//
+//    while number >= 10 {
+//        digitSum += number % 10
+//        number /= 10
+//    }
+//    digitSum += number
+//
+//    return digitSum
+//}
 func digitalRoot(of number: Int) -> Int {
-    var number = number
-    
-    while number >= 10 {
-        number = sumDigit(number)
-    }
-    
-    return number
-}
-
-func sumDigit(_ number: Int) -> Int {
     var digitSum = 0
     var number = number
     
@@ -26,12 +38,20 @@ func sumDigit(_ number: Int) -> Int {
         digitSum += number % 10
         number /= 10
     }
+    
     digitSum += number
+    
+    if digitSum < 10 {
+        return digitSum
+    }
+    
+    digitSum = digitalRoot(of: digitSum)
     
     return digitSum
 }
 
 assert(digitalRoot(of: 16) == 7)
+assert(digitalRoot(of: 456) == 6)
 assert(digitalRoot(of: 942) == 6)
 assert(digitalRoot(of: 132189) == 6)
 assert(digitalRoot(of: 493193) == 2)
