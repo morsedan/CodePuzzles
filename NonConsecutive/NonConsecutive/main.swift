02951a7ceb5f61883711bd10021248fff7f0c2cb
Dan Morse <djmthedev@gmail.com>
2020-11-28 13:45:50 -0800

    added tests

--- NonConsecutive/NonConsecutive/main.swift
+++ NonConsecutive/NonConsecutive/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+func allNonConsecutive (_ arr: [Int]) -> [(Int, Int)] {
+  return []
+}
 
+func test_allNonConsecutive() {
+    func equal(_ first: [(Int, Int)], _ second: [(Int, Int)]) -> Bool{
+        if first.count != second.count {
+            return false
+        }
+        for (index, pair) in first.enumerated() {
+            if pair.0 != second[index].0 ||
+                pair.1 != second[index].1 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func check(_ nums: [Int], _ solution: [(Int, Int)]) {
+        assert(equal(allNonConsecutive(nums), solution))
+    }
+    
+    var nums = [1,2,3,4,6,7,8,10]
+    var solution = [(4, 6), (7, 10)]
+    check(nums, solution)
+    nums = [1,2,3,4,6,7,8,15,16]
+    solution = [( 4, 6 ), ( 7, 15 )]
+    check(nums, solution)
+}
+
+test_allNonConsecutive()
+print("passed!")
