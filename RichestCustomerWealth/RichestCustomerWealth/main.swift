7b2c5ff334a48efce65f97a1223404a0b798434c
Dan Morse <djmthedev@gmail.com>
2020-12-12 09:04:38 -0800

    added RichestCustomerWealth

---
+++ RichestCustomerWealth/RichestCustomerWealth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RichestCustomerWealth
+//
+//  Created by morse on 12/12/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
