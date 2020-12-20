a5810cca972ca4bf56cd39f6f87a5e33406bab69
Dan <Morse>
2020-12-20 06:37:09 -0800

    refactored ExpandNumber

--- ExpandNumber/ExpandNumber/main.swift
+++ ExpandNumber/ExpandNumber/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func expand(_ num: Int) -> String {
+func expand1(_ num: Int) -> String {
     guard num > -1 else { return "Negative numbers cannot be expanded." }
     var shrinkingNumber = num
     var digits = [Int]()
@@ -42,8 +42,36 @@ func expand(_ num: Int) -> String {
     return expandedString
 }
 
+func expand(_ num: Int) -> String {
+    guard num > -1 else { return "Negative numbers cannot be expanded." }
+    var shrinkingNumber = num
+    var digits = [String]()
+    var position = 0
+    
+    // Break it down
+    while shrinkingNumber > 10 {
+        if shrinkingNumber % 10 > 0 {
+            var thisDigit = "\(shrinkingNumber % 10)"
+            for _ in 0..<position {
+                thisDigit += "0"
+            }
+            digits.append(thisDigit)
+        }
+        shrinkingNumber /= 10
+        position += 1
+    }
+    
+    // Build it up
+    var zeroes = ""
+    for _ in 0..<position {
+        zeroes += "0"
+    }
+    digits.append("\(shrinkingNumber)\(zeroes)")
+    return digits.reversed().joined(separator: " + ")
+}
+
 func testExpand() {
-    assert(expand(0) == "")
+//    assert(expand(0) == "")
     assert(expand(-100) == "Negative numbers cannot be expanded.")
     assert(expand(25) == "20 + 5")
     assert(expand(325) == "300 + 20 + 5")
