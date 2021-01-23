b12084c87a9fe1d0e311a04d9bde2c4c1096d498
Dan Morse <djmthedev@gmail.com>
2021-01-23 07:42:56 -0800

    implemented disemvowel

---
+++ Disemvowell/Disemvowell/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Disemvowell
+//
+//  Created by morse on 1/23/21.
+//
+
+import Foundation
+
+func disemvowel(_ s: String) -> String {
+    var result = ""
+    for letter in s {
+        if !["a", "e", "i", "o", "u"].contains(letter.lowercased()) {
+            result.append(letter)
+        }
+    }
+    return result
+}
+
+func test() {
+    assert("Ths wbst s fr lsrs LL!" == disemvowel("This website is for losers LOL!"))
+    assert("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd" == disemvowel("No offense but,\nYour writing is among the worst I've ever read"))
+    assert("Wht r y,  cmmnst?" == disemvowel("What are you, a communist?"))
+}
+
+test()
+print("passed!")
