5409e2b044f1600423afea5a59ee371c6e7752fc
Dan Morse <djmthedev@gmail.com>
2021-02-06 11:50:37 -0800

    added solution to PrimeNumbers

--- PrimeNumbers/PrimeNumbers/main.swift
+++ PrimeNumbers/PrimeNumbers/main.swift
@@ -8,27 +8,27 @@
 
 import Foundation
 
-var primes: Set<Int> = [2, 3]
-extension Int {
-    func isPrime() -> Bool {
-        if self < 2 {
-            return false
-        }
-        if primes.contains(self) {
-            return true
-        } else {
-            for number in 2...Int(Double(self).squareRoot()) {
-                if self % number == 0 {
-                    return false
-                }
-            }
-        }
-        primes.insert(self)
-        return true
-    }
-}
+//var primes: Set<Int> = [2, 3]
+//extension Int {
+//    func isPrime() -> Bool {
+//        if self < 2 {
+//            return false
+//        }
+//        if primes.contains(self) {
+//            return true
+//        } else {
+//            for number in 2...Int(Double(self).squareRoot()) {
+//                if self % number == 0 {
+//                    return false
+//                }
+//            }
+//        }
+//        primes.insert(self)
+//        return true
+//    }
+//}
 
-func getPrimes(from start: Int, to end: Int) -> [Int] {
+func getPrimes2(from start: Int, to end: Int) -> [Int] {
     var result: [Int] = []
     let numbers = start > end ? end...start : start...end
 
@@ -41,6 +41,23 @@ func getPrimes(from start: Int, to end: Int) -> [Int] {
     return result
 }
 
+extension Int {
+    func isPrime() -> Bool {
+        guard self != 2, self != 3 else { return true }
+        guard self != 1, self % 6 == 1 || self % 6 == 5 else { return false }
+        
+        for n in Swift.stride(from: 5, through: Int(sqrt(Double(self))), by: 2) {
+            guard self % n != 0 else { return false }
+        }
+        
+        return true
+    }
+}
+
+func getPrimes(from start: Int, to end: Int) -> [Int] {
+    return (min(start, end)...max(start, end)).filter { $0.isPrime() }
+}
+
 func test() {
     assert(2.isPrime() == true)
     assert(10.isPrime() == false)
@@ -48,7 +65,6 @@ func test() {
     assert(getPrimes(from: 0, to: 30) == [2, 3,5, 7, 11, 13, 17, 19, 23, 29])
     assert(getPrimes(from: 30, to: 0) == [2, 3,5, 7, 11, 13, 17, 19, 23, 29])
 }
-print(15.isPrime())
-print(getPrimes(from: 0, to: 30))
+
 test()
 print("passed!")
