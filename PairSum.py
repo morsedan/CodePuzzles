f726bf8eb0abe8c56b8447cfe5fb54aba151da17
Dan Morse <djmthedev@gmail.com>
2021-02-27 11:04:55 -0800

    added solution for PairSum

--- PairSum.py
+++ PairSum.py
@@ -17,7 +17,7 @@ FUNCTION SO IT OUTPUTS THE NUMBER OF PAIRS
 """
 import unittest
 
-def pair_sum(arr, k):
+def pair_sum1(arr, k):
     num_dict = {}
     result = []
 
@@ -42,10 +42,29 @@ def pair_sum(arr, k):
 
     return len(result)
 
+def pair_sum(arr, k):
+    if len(arr) < 2:
+        return
+
+    seen = set()
+    output = set()
+
+    for num in arr:
+
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+
+        else:
+            output.add( ((min(num,target)), max(num, target)) )
+
+    # print('\n'.join(map(str,list(output))))
+    return len(output)
+
 class PairSumTest(unittest.TestCase):
 
     def test_pair_sum(self, sol):
-
         self.assertEqual(sol([1, 2, 3, 1], 3), 1)
         self.assertEqual(sol([1, 3, 2, 2], 4), 2)
         self.assertEqual(sol([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10), 6)
