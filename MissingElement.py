6d5574c986dcef666b72e179589f1cd1a9fea3b0
Dan Morse <djmthedev@gmail.com>
2021-03-01 09:04:32 -0800

    added tests

---
+++ MissingElement.py
@@ -0,0 +1,25 @@
+T = TestFinder()
+
+
+def finder(arr1, arr2):
+    pass
+
+
+
+arr1 = [1,2,3,4,5,6,7]
+arr2 = [3,7,2,1,4,6]
+print(finder(arr1,arr2))
+
+import unittest
+class TestFinder(unittest.TestCase):
+
+    def test(self, sol):
+        self.assertEqual(sol([5, 5, 7, 7], [5, 7, 7]), 5)
+        self.assertEqual(sol([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
+        self.assertEqual(sol([9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 8, 7, 5, 4, 3, 2, 1]), 6)
+        print('ALL TEST CASES PASSED')
+
+
+# Run test
+t = T
+t.test(finder)
