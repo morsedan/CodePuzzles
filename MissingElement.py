028beeb4a31b797ba62011e5d679771cbd201011
Dan Morse <djmthedev@gmail.com>
2021-03-01 09:29:53 -0800

    implemented MissingElement

--- MissingElement.py
+++ MissingElement.py
@@ -1,11 +1,33 @@
 def finder(arr1, arr2):
-    pass
+    elementDict1 = {}
+    elementDict2 = {}
+
+    for element in arr1:
+        if element in elementDict1:
+            elementDict1[element] += 1
+        else:
+            elementDict1[element] = 1
+
+    for element in arr2:
+        if element in elementDict2:
+            elementDict2[element] += 1
+        else:
+            elementDict2[element] = 1
+
+    for item in elementDict1:
+        if item not in elementDict2:
+            return item
+        elif elementDict1[item] > elementDict2[item]:
+            return item
+
+    return -1
+
 
 
 
 arr1 = [1,2,3,4,5,6,7]
 arr2 = [3,7,2,1,4,6]
-print(finder(arr1,arr2))
+# print("here", finder(arr1,arr2))
 
 import unittest
 class TestFinder(unittest.TestCase):
