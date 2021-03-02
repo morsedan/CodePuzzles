440f185ce67118ea16de0d4edc81c0731fa9d0f5
Dan Morse <djmthedev@gmail.com>
2021-03-02 09:29:34 -0800

    added solution code

--- LargestSubarray.py
+++ LargestSubarray.py
@@ -1,4 +1,16 @@
 def large_cont_sum(arr):
+    if len(arr) == 0:
+        return 0
+
+    max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(current_sum+num, num)
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
+
+def large_cont_sum1(arr):
     if arr == []:
         return 0
 
