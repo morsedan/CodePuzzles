0fa26e18d66d70c50d7910c51ecc8f53d350b005
Dan Morse <djmthedev@gmail.com>
2021-04-03 10:00:34 -0700

    refactored MonMon

--- MonMon.py
+++ MonMon.py
@@ -1,9 +1,11 @@
 def calculate_years(principal, interest, tax, desired):
     years = 0
+    net_interest = interest - (interest * tax)
+    earned = 0
     while principal < desired:
-        earnings = principal * interest
-        net = earnings - earnings * tax
-        principal += net
+        earned += principal * net_interest
+        principal += earned
+        earned = 0
         years += 1
     return years
 
@@ -13,3 +15,4 @@ t = 0.18
 d = 1100
 
 print(calculate_years(p, i, t, d))
+print(calculate_years(1000,0.01625,0.18,1200), 14)
