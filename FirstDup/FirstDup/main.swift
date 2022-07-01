d68bb64aa5412500e16df85daeb6e518c02f8e7d
Dan Morse <dmorse@adobe.com>
2022-06-30 19:40:33 -0700

    first duplicate

--- FirstDup/FirstDup/main.swift
+++ FirstDup/FirstDup/main.swift
@@ -1,11 +1,39 @@
-//
-//  main.swift
-//  FirstDup
-//
-//  Created by Dan Morse on 6/30/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+var numbers: [Int] = []
+for i in 1...10_000_000 {
+    numbers.append(i)
+}
+numbers.append(4)
 
+func firstDuplicate1(nums: [Int]) -> Int {
+    var visited: [Int] = [nums[0]]
+    for i in 1..<nums.count {
+        print(i)
+        for y in 0..<visited.count {
+            if nums[i] == visited[y] {
+                return nums[i]
+            } else {
+                visited.append(nums[i])
+            }
+        }
+    }
+    return -1
+}
+
+func firstDuplicate(nums: [Int]) -> Int {
+    var visited: Set<Int> = []
+    for index in 0..<nums.count {
+        print(index)
+        if visited.contains(nums[index]) {
+            return nums[index]
+        } else {
+            visited.insert(nums[index])
+        }
+    }
+    return -1
+}
+
+
+assert(firstDuplicate(nums: numbers) == 4)
+print("Tests passed!")
